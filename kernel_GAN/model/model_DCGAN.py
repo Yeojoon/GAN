d3426ab0e2e21cf1bf2909d19908d816d128bf08
Yeojoon <47948859+Yeojoon@users.noreply.github.com>
2020-05-18 15:50:11 -0400

    Update model_DCGAN.py

--- kernel_GAN/model/model_DCGAN.py
+++ kernel_GAN/model/model_DCGAN.py
@@ -10,11 +10,19 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
+
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(mean, std)
+        m.bias.data.zero_()
+        
+        
 # G(z)
-class generator(nn.Module):
+class generator_mnist(nn.Module):
     # initializers
     def __init__(self, d=128):
-        super(generator, self).__init__()
+        super(generator_mnist, self).__init__()
         self.deconv1 = nn.ConvTranspose2d(100, d*8, 4, 1, 0)
         self.deconv1_bn = nn.BatchNorm2d(d*8)
         self.deconv2 = nn.ConvTranspose2d(d*8, d*4, 4, 2, 1)
@@ -40,36 +48,103 @@ class generator(nn.Module):
         x = nn.Tanh()(self.deconv5(x))
 
         return x
+        
 
-class discriminator(nn.Module):
-    # initializers
-    def __init__(self, d=128):
-        super(discriminator, self).__init__()
-        self.conv1 = nn.Conv2d(1, d, 4, 2, 1)
-        self.conv2 = nn.Conv2d(d, d*2, 4, 2, 1)
-        self.conv2_bn = nn.BatchNorm2d(d*2)
-        self.conv3 = nn.Conv2d(d*2, d*4, 4, 2, 1)
-        self.conv3_bn = nn.BatchNorm2d(d*4)
-        self.conv4 = nn.Conv2d(d*4, d*8, 4, 2, 1)
-        self.conv4_bn = nn.BatchNorm2d(d*8)
-        self.conv5 = nn.Conv2d(d*8, 1, 4, 1, 0)
+# helper deconv function
+def deconv(in_channels, out_channels, kernel_size, stride=2, padding=1, batch_norm=True):
+    """Creates a transposed-convolutional layer, with optional batch normalization.
+    """
+    # create a sequence of transpose + optional batch norm layers
+    layers = []
+    transpose_conv_layer = nn.ConvTranspose2d(in_channels, out_channels, 
+                                              kernel_size, stride, padding, bias=False)
+    # append transpose convolutional layer
+    layers.append(transpose_conv_layer)
+    
+    if batch_norm:
+        # append batchnorm layer
+        layers.append(nn.BatchNorm2d(out_channels))
+        
+    return nn.Sequential(*layers)
 
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
 
-    # forward method
-    def forward(self, input):
-        x = F.leaky_relu(self.conv1(input), 0.2)
-        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
-        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
-        x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
-        x = nn.Sigmoid()(self.conv5(x))
 
-        return x
+class generator_svhn(nn.Module):
+    
+    def __init__(self, z_size, conv_dim=32):
+        super(generator_svhn, self).__init__()
 
-def normal_init(m, mean, std):
-    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
-        m.weight.data.normal_(mean, std)
-        m.bias.data.zero_()
+        # complete init function
+        
+        self.conv_dim = conv_dim
+        
+        # first, fully-connected layer
+        self.fc = nn.Linear(z_size, conv_dim*4*4*4)
+
+        # transpose conv layers
+        self.t_conv1 = deconv(conv_dim*4, conv_dim*2, 4)
+        self.t_conv2 = deconv(conv_dim*2, conv_dim, 4)
+        self.t_conv3 = deconv(conv_dim, 3, 4, batch_norm=False)
+        
+
+    def forward(self, x):
+        # fully-connected + reshape 
+        x = self.fc(x)
+        x = x.view(-1, self.conv_dim*4, 4, 4) # (batch_size, depth, 4, 4)
+        
+        # hidden transpose conv layers + relu
+        x = F.relu(self.t_conv1(x))
+        x = F.relu(self.t_conv2(x))
+        
+        # last layer + tanh activation
+        x = self.t_conv3(x)
+        out = nn.Tanh()(x)
+        
+        return out
+
+    
+# helper conv function
+def conv(in_channels, out_channels, kernel_size, stride=2, padding=1, batch_norm=True):
+    """Creates a convolutional layer, with optional batch normalization.
+    """
+    layers = []
+    conv_layer = nn.Conv2d(in_channels, out_channels, 
+                           kernel_size, stride, padding, bias=False)
+    
+    # append conv layer
+    layers.append(conv_layer)
+
+    if batch_norm:
+        # append batchnorm layer
+        layers.append(nn.BatchNorm2d(out_channels))
+     
+    # using Sequential container
+    return nn.Sequential(*layers)
+
+    
+    
+class encoder_svhn(nn.Module):
+    
+    def __init__(self, z_size, conv_dim=32):
+        super(encoder_svhn, self).__init__()
+        
+        self.conv_dim = conv_dim
+        
+        self.conv1 = conv(3, conv_dim, 4, batch_norm=False)
+        self.conv2 = conv(conv_dim, conv_dim*2, 4)
+        self.conv3 = conv(conv_dim*2, conv_dim*4, 4)
+        
+        self.fc = nn.Linear(conv_dim*4*4*4, z_size)
+        
+        
+    def forward(self, x):
+        
+        x = F.leaky_relu(self.conv1(x), 0.2)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        x = F.leaky_relu(self.conv3(x), 0.2)
+        
+        x = x.view(-1, self.conv_dim*4*4*4)
+        
+        out = self.fc(x)
+        
+        return out
